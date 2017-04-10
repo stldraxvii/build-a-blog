0e866315def33895cf567416d674a005f0385af4
Matt Kemper <matt.kemper90@gmail.com>
2017-04-10 17:51:05 -0500

    Added basic functionality; 2 different routes

--- main.py
+++ main.py
@@ -15,11 +15,58 @@
 # limitations under the License.
 #
 import webapp2
+import jinja2
+import os
 
-class MainHandler(webapp2.RequestHandler):
+from google.appengine.ext import db
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
+
+
+class Handler(webapp2.RequestHandler):
+    def write (self, *a, **kw):
+        self.response.out.write(*a,**kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template,**kw):
+        self.write(self.render_str(template, **kw))
+
+    def render_blog(self, title="", body="", error=""):
+        posts = db.GqlQuery("SELECT * FROM Post ORDER BY created DESC LIMIT 5")
+        self.render("blog.html", title=title, body=body, error=error, posts=posts)
+
+class Post(db.Model):
+    title = db.StringProperty(required = True)
+    body = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+
+
+class MainHandler(Handler):
     def get(self):
-        self.response.write('Hello world!')
+        self.render("form.html")
+
+    def post(self):
+        title = self.request.get("title")
+        body = self.request.get("body")
+
+        if title and body:
+            a = Post(title = title, body = body)
+            a.put()
+            self.redirect("/blog")
+        else:
+            error = "We need both a title and a body for your post!"
+            self.render_front(title, body, error)
+
+class BlogHandler(Handler):
+    def get(self):
+        self.render_blog()
+
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/blog', BlogHandler)
 ], debug=True)
